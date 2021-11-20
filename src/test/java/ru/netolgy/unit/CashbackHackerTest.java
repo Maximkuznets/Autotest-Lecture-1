e192fe55a9fa586cf623a80b2215d63086cb92b0
Кузнецов Максим <maximkuznetsov17250@gmail.com>
2021-11-20 18:04:42 +0300

    add branch junit4

--- src/test/java/ru/netolgy/unit/CashbackHackerTest.java
+++ src/test/java/ru/netolgy/unit/CashbackHackerTest.java
@@ -1,8 +1,8 @@
 package ru.netolgy.unit;
 
-import org.testng.annotations.Test;
+import org.junit.Test;
 
-import static org.testng.Assert.*;
+import static org.junit.Assert.*;
 
 public class CashbackHackerTest {
 
@@ -12,7 +12,7 @@ public class CashbackHackerTest {
         int amount = 1200;
         int actual = service.remain(amount);
         int expected = 800;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
@@ -21,7 +21,7 @@ public class CashbackHackerTest {
         int amount = 800;
         int actual = service.remain(amount);
         int expected = 200;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 
     @Test
@@ -30,6 +30,6 @@ public class CashbackHackerTest {
         int amount = 1000;
         int actual = service.remain(amount);
         int expected = 0;
-        assertEquals(actual, expected);
+        assertEquals(expected, actual);
     }
 }
